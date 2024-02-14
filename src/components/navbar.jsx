fed42a6f9cdb1251abdbf79d4eddeeccca15542b
anirudhg001 <annigupta007@gmail.com>
2024-02-14 09:40:53 +0530

    navbar md: responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,27 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 const navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const links = [
+    { URL: "/", title: "Home" },
+    { URL: "/about", title: "About" },
+    { URL: "/portfolio", title: "Portfolio" },
+    { URL: "/contact", title: "Contact" },
+  ];
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="hidden md:flex gap-4">
+        {links.map((link) => (
+          <Link href={link.URL} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
       {/* LOGO */}
-      <div className="">
+      <div className="md:hidden">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -17,13 +34,27 @@ const navbar = () => {
       </div>
 
       {/* RESPONSIVE MENU */}
-      <div className="">
+      <div className="md:hidden">
         {/* MENU BUTTON */}
-        <button className="w-10 h-8 flex flex-col justify-between">
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+        <button
+          className="relative w-10 h-8 flex flex-col justify-between z-50"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
         </button>
+
+        {/* MENU LIST  */}
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.URL} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
