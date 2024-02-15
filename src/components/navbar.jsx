49e4b34b02988a0eef58635e1da46c9fd8091943
anirudhg001 <annigupta007@gmail.com>
2024-02-15 22:35:48 +0530

    navlink

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
+import NavLink from "./navLink";
 
 const navbar = () => {
   const [open, setOpen] = useState(false);
@@ -17,9 +18,7 @@ const navbar = () => {
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
-          <Link href={link.URL} key={link.title}>
-            {link.title}
-          </Link>
+          <NavLink link={link} key={link.title} />
         ))}
       </div>
       {/* LOGO */}
