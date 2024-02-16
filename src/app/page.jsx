f7374ec220a74c24eef5cf0436d0061e85483d0d
anirudhg001 <annigupta007@gmail.com>
2024-02-16 22:21:32 +0530

    hero section ui: mediary commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,16 +2,14 @@ import Image from "next/image";
 
 const Homepage = () => {
   return (
-    <div className="h-full flex flex-col">
+    <div className="h-full flex flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* IMAGE CONTAINER  */}
       <div className="h-1/2 relative">
         <Image src="/hero.png" alt="" fill className="object-contain" />
       </div>
       {/* TEXT CONTAINER */}
       <div className="h-1/2 flex flex-col items-center justify-center gap-8">
-        <h1 className="text-4xl font-bold">
-          Crafting Digital Experiences, Designing Tomorrow
-        </h1>
+        <h1 className="text-4xl font-bold">Crafting Digital Experiences</h1>
         <p>
           Welcome to my digital canvas, where innovation and creativity
           converge. With a keen eye for aesthetics and enthusiasm for code, my
@@ -19,8 +17,12 @@ const Homepage = () => {
           commitment to excellence.
         </p>
         <div className="flex gap-4">
-          <button>View My Work</button>
-          <button>Contact Me</button>
+          <button className="p-4 ring-1 ring-black rounded-lg bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 ring-1 ring-black rounded-lg">
+            Contact Me
+          </button>
         </div>
       </div>
     </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -60,7 +60,7 @@ const navbar = () => {
         {open && (
           <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
             {links.map((link) => (
-              <Link href={link.URL} key={link.title}>
+              <Link href={link.url} key={link.title}>
                 {link.title}
               </Link>
             ))}
