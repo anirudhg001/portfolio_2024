0903215e41fac67e376124da7fa2d21a4be63286
anirudhg001 <annigupta007@gmail.com>
2024-02-22 22:57:16 +0530

    fix: menu z-index

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -58,7 +58,7 @@ const navbar = () => {
 
         {/* MENU LIST  */}
         {open && (
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+          <div className="absolute z-10 top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
             {links.map((link) => (
               <Link href={link.url} key={link.title}>
                 {link.title}
