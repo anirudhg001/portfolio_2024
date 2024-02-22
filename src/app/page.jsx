b67a692c51cb93c32279a779a2f3ff91cbd0a0e5
anirudhg001 <annigupta007@gmail.com>
2024-02-22 18:53:37 +0530

    hero css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const Homepage = () => {
   return (
-    <div className="h-full flex flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex flex-col mt-0 lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* IMAGE CONTAINER  */}
-      <div className="h-1/2 relative">
-        <Image src="/hero.png" alt="" fill className="object-contain" />
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src="/hero1.png" alt="" fill className="object-contain" />
       </div>
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 flex flex-col items-center justify-center gap-8">
+      <div className="h-1/2 flex flex-col items-center justify-center gap-6 lg:h-full lg:w-1/2">
         <h1 className="text-4xl md:text-6xl font-bold">
           Creative Web Developer
         </h1>
