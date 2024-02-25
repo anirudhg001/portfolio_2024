ab5c990f66c7c0a58cbd134a6f54cdf161288b38
anirudhg001 <annigupta007@gmail.com>
2024-02-25 22:23:51 +0530

    menu animation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,6 +8,13 @@ import { motion } from "framer-motion";
 const navbar = () => {
   const [open, setOpen] = useState(false);
 
+  const links = [
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About" },
+    { url: "/portfolio", title: "Portfolio" },
+    { url: "/contact", title: "Contact" },
+  ];
+
   const topVariants = {
     closed: {
       rotate: 0,
@@ -36,12 +43,26 @@ const navbar = () => {
     },
   };
 
-  const links = [
-    { url: "/", title: "Home" },
-    { url: "/about", title: "About" },
-    { url: "/portfolio", title: "Portfolio" },
-    { url: "/contact", title: "Contact" },
-  ];
+  const listVariants = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: "0",
+    },
+  };
+
+  const listItemVariants = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
@@ -82,27 +103,39 @@ const navbar = () => {
         >
           <motion.div
             variants={topVariants}
-            className="w-10 h-1 bg-black rounded"
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
           ></motion.div>
           <motion.div
             variants={centerVariants}
+            animate={open ? "opened" : "closed"}
             className="w-10 h-1 bg-black rounded"
           ></motion.div>
           <motion.div
             variants={bottomVariants}
-            className="w-10 h-1 bg-black rounded"
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
           ></motion.div>
         </button>
 
         {/* MENU LIST  */}
         {open && (
-          <div className="absolute z-10 top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+          <motion.div
+            variants={listVariants}
+            initial="closed"
+            animate="opened"
+            className="absolute z-10 top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl"
+          >
             {links.map((link) => (
-              <Link href={link.url} key={link.title}>
-                {link.title}
-              </Link>
+              <motion.div
+                variants={listItemVariants}
+                className=""
+                key={link.title}
+              >
+                <Link href={link.url}>{link.title}</Link>
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
