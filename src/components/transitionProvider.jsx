1d383e562f949ffb01d6ce794bcefab2e443c351
anirudhg001 <annigupta007@gmail.com>
2024-02-27 22:13:51 +0530

    animate: desktop view

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -2,11 +2,24 @@
 
 import { AnimatePresence } from "framer-motion";
 import Navbar from "./navbar";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
-const transitionProvider = () => {
+const transitionProvider = ({ children }) => {
+  const pathName = usePathname();
+
   return (
     <AnimatePresence>
-      <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
+      <div
+        key="pathName"
+        className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100"
+      >
+        <motion.div
+          className="h-screen w-screen bg-black fixed rounded-b-[100px] z-40"
+          animate={{ height: "0vh" }}
+          exit={{ height: "100vh" }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        />
         <div className="h-24">
           <Navbar />
         </div>
