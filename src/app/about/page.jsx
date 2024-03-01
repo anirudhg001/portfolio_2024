5c2c593f95045470e4d9ffe66758a6193d92d212
anirudhg001 <annigupta007@gmail.com>
2024-03-01 21:29:37 +0530

    timeline and experience section complete

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,21 @@
 "use client";
-import { motion } from "framer-motion";
 
-function AboutPage() {
+import { motion, useInView, useScroll } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
+
+const AboutPage = () => {
+  const containerRef = useRef();
+
+  const { scrollYProgress } = useScroll({ container: containerRef });
+
+  const skillRef = useRef();
+  // const isSkillRefInView = useInView(skillRef, {once:true});
+  const isSkillRefInView = useInView(skillRef, { margin: "-100px" });
+
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" });
+
   return (
     <motion.div
       className="h-full"
@@ -124,51 +138,126 @@ function AboutPage() {
           </motion.svg>
         </div>
         {/* EXPERIENCE CONTAINER */}
-        <div className="flex flex-col gap-12 justify-center">
+        <div
+          className="flex flex-col gap-12 justify-center pb-48"
+          ref={experienceRef}
+        >
           {/* EXPERIENCE TITLE */}
-          <h1 className="font-bold text-2xl">EXPERIENCE</h1>
+          <motion.h1
+            initial={{ x: "-300px" }}
+            animate={isExperienceRefInView ? { x: "0" } : {}}
+            transition={{ delay: 0.2 }}
+            className="font-bold text-2xl"
+          >
+            EXPERIENCE
+          </motion.h1>
           {/* EXPERIENCE LIST */}
-          <div className="">
+          <motion.div
+            initial={{ x: "-300px" }}
+            animate={isExperienceRefInView ? { x: "0" } : {}}
+            className=""
+          >
             {/* EXPERIENCE LIST ITEM */}
-            <div className="flex justify-between h-48 ">
+            <div className="flex justify-between h-48">
               {/* LEFT */}
-              <div className="w-1/3">
+              <div className="w-1/3 ">
                 {/* JOB TITLE */}
-                <div className="bg-white p-3 rounded-b-lg rounded-s-lg">
-                  Technology Analyst Intern
+                <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                  Senior JavaScript Engineer
                 </div>
                 {/* JOB DESC */}
                 <div className="p-3 text-sm italic">
-                  Worked on the frontend , backend and designing of the website.
-                  Developed and deployed two fully funtional blog websites.
+                  I led web development, offering expertise in JavaScript
+                  frameworks.{" "}
                 </div>
                 {/* JOB DATE */}
-                <div className="text-red-400 text-sm font-semibold">
-                  July,23 - Jan,24
+                <div className="p-3 text-red-400 text-sm font-semibold">
+                  2024 - Present
                 </div>
-                {/* COMPANY */}
-                <div className="p-1 rounded bg-white text-sm font-semibold">
-                  DefineRight Pvt. Ltd.
+                {/* JOB COMPANY */}
+                <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                  Apple
                 </div>
               </div>
               {/* CENTER */}
-              <div className="w-1/6">
+              <div className="w-1/6 flex justify-center">
                 {/* LINE */}
-                <div>
+                <div className="w-1 h-full bg-gray-600 rounded relative">
                   {/* LINE CIRCLE */}
-                  <div></div>
+                  <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                 </div>
               </div>
-
               {/* RIGHT */}
               <div className="w-1/3 "></div>
             </div>
-          </div>
+            {/* EXPERIENCE LIST ITEM */}
+            <div className="flex justify-between h-48">
+              {/* LEFT */}
+              <div className="w-1/3 "></div>
+              {/* CENTER */}
+              <div className="w-1/6 flex justify-center">
+                {/* LINE */}
+                <div className="w-1 h-full bg-gray-600 rounded relative">
+                  {/* LINE CIRCLE */}
+                  <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                </div>
+              </div>
+              {/* RIGHT */}
+              <div className="w-1/3 ">
+                {/* JOB TITLE */}
+                <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                  Senior React Developer
+                </div>
+                {/* JOB DESC */}
+                <div className="p-3 text-sm italic">
+                  I spearheaded React-based application development, leveraging
+                  advanced skills.{" "}
+                </div>
+                {/* JOB DATE */}
+                <div className="p-3 text-red-400 text-sm font-semibold">
+                  2019 - 2024{" "}
+                </div>
+                {/* JOB COMPANY */}
+                <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                  Microsoft
+                </div>
+              </div>
+            </div>
+            {/* EXPERIENCE LIST ITEM */}
+            <div className="flex justify-between h-48">
+              {/* LEFT */}
+              <div className="w-1/3 ">
+                {/* JOB TITLE */}
+                <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                  Freelancer{" "}
+                </div>
+                {/* JOB DESC */}
+                <div className="p-3 text-sm italic">
+                  I provided web solutions, applying a range of technologies to
+                  address client requirements.{" "}
+                </div>
+                {/* JOB DATE */}
+                <div className="p-3 text-red-400 text-sm font-semibold">
+                  2010 - 2019{" "}
+                </div>
+              </div>
+              {/* CENTER */}
+              <div className="w-1/6 flex justify-center">
+                {/* LINE */}
+                <div className="w-1 h-full bg-gray-600 rounded relative">
+                  {/* LINE CIRCLE */}
+                  <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                </div>
+              </div>
+              {/* RIGHT */}
+              <div className="w-1/3 "></div>
+            </div>
+          </motion.div>
         </div>
       </div>
       {/* SVG CONTAINER */}
       <div className="hidden"></div>
     </motion.div>
   );
-}
+};
 export default AboutPage;
